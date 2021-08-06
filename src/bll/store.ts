211eb149a1687b8e31eb7b0406a7081eb76e753a
Vladislav V <0428vlad0428@mail.ru>
2021-08-06 15:33:21 +0300

    localstorage into Redux

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,12 +1,21 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
+import thunk from "redux-thunk";
+import {loadState, saveState} from "../utils/localstorage-utils";
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
 
+export const store = createStore(rootReducer, loadState(), applyMiddleware(thunk))
+
+//below saving data in localStorage
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    })
+})
+
+
 export type AppStateType = ReturnType<typeof rootReducer>
-
-export const store = createStore(rootReducer)
-
-type AppStoreType = typeof store
+export type AppStoreType = typeof store
